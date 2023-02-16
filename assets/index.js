a656f86b316a125bc1279d16048ec116aa508cdf
Ralph Andy Menz <andyralphmenz@gmail.com>
2023-02-16 23:49:02 +0000

    progress

--- assets/index.js
+++ assets/index.js
@@ -5,4 +5,9 @@ function closeMenu(){
     document.body.classList.remove('menu--open')
     }     
     
- 
+ function openDrop(){
+    document.drop__para.classList += " open__drop"
+ }
+ function closeDrop(){
+    document.drop__para.classList.remove('open--drop')
+ }
